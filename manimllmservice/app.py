520b3cd7f0e9b0ceeb36eef785dc93aa5ac46224
sourize <sourishchatterjee0067@gmail.com>
2025-06-07 11:00:25 +0530

    Updated Code

--- manimllmservice/app.py
+++ manimllmservice/app.py
@@ -47,7 +47,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_methods=["*"],
+    allow_methods=["GET","POST","OPTIONS"],
     allow_headers=["*"],
 )
 
